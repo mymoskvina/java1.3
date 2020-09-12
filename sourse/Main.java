4e3dac6ad9bf0547cc6f04f3117bfff70c83870c
Elena <mymoskvina@gmail.com>
2020-09-12 18:59:00 +0300

    Initial commit

---
+++ sourse/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args){
+        int deposit = 2_000_000_000;
+        int money_transfer = 500_000_000;
+        int total = deposit + money_transfer;
+        System.out.println(total);
+    }
+}
